482f784fb471922e0ff0f6f7ae1b535c88ccb1cf
lionnt01 <lionnt01@lionnt01-mbp13.wireless.nyumc.org>
2023-04-12 08:39:56 -0400

    added doc header to clusterAnalysisFromSegmentedNuclei.ijm

--- matlab/FullAnalysisScript.m
+++ matlab/FullAnalysisScript.m
@@ -1,5 +1,6 @@
 fijiOutFolder = '/Users/lionnt01/Documents/data/feiyue/egg chamber image quant/testOut2';
 fijiOutFolder = '/Users/lionnt01/Documents/data/feiyue/clusterImgAnalysis/out';
+fijiOutFolder = '/Users/lionnt01/Documents/data/feiyue/testSer5ph/nucSeg20_3img';
 
 % intitialize object 
 ec = eggChamberDataFolder(fijiOutFolder);
@@ -14,8 +15,8 @@ t = backgroundCorrectNucIntensity(t);
 t = assignNucleiToGroupsWholeTable(t);
 
 %% plot nuclei stats by group
-%plotNucleiStatsByGroup(t,'nucVolume');
+plotNucleiStatsByGroup(t,'nucVolume');
 
-%t = addvars(t,t.nucC1_Mean_wholeImgCorr.*t.nucVolume,'NewVariableNames','integratedDAPI');
+t = addvars(t,t.nucC1_Mean_wholeImgCorr.*t.nucVolume,'NewVariableNames','integratedDAPI');
 plotNucleiStatsByGroup(t,'integratedDAPI');
 %%
